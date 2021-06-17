b7ffbf9ec20ce308b99f2d02d00633b60829b77a
shambhavisud <67536519+shambhavisud@users.noreply.github.com>
2021-06-17 12:28:30 +0530

    added query selector

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+let btnTranslate = document.querySelector("#btn-translate");
+console.log(btnTranslate);
+//   console.log("input", txtInput.value);
+let clickHandler = () => {
+  console.log("clicked!");
+};
+// function clickHandler() {
+//   console.log("clicked!");
+//   console.log("input", txtInput.value);
+// }
 
+btnTranslate.addEventListener("click", clickHandler);
